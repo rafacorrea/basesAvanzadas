d0836830c17671a81e75c4ad4ee39c4e8e669cb2
rafa <rafael_correa_3@hotmail.com>
2015-08-27 23:31:36 -0500

    i

---
+++ tarea1/ejercicio1.c
@@ -0,0 +1,293 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    char* nombre;
+    char* apellidos;
+	int edad;
+    char* rol;
+} Persona;
+
+typedef struct
+{
+    char * nombre;
+    int eslora;
+    int manga;
+    int maximo;
+    Persona * tripulacion;
+    Persona * propietario;
+} Barco;
+
+void agregarProp(Persona ** p, Barco ** b);
+void agregarTrip(Persona ** p, Barco ** b);
+void agregarPersona(Persona ** p);
+void verPersonas(Persona *p);
+void verBarcos(Barco *b);
+int cuantosBarcos(Barco * b);
+void agregarBarco(Barco ** b);
+void borrar(Persona ** p, int aBorrar);
+void freeMemory(Persona * p, Barco * b);
+
+main()
+{
+    printf("Seleccione su opcion: \n1-Agregar Barco\n2-Agregar Persona\n3-Agregar Persona a Barco\n4-Mostrar Barcos\n5-Mostrar Personas\n");
+    int decision;
+    
+    scanf("%d", &decision);
+    char * nombre; 
+    int temp;
+
+    
+    Persona *p;
+    Barco *b;
+
+    b=NULL;//(Barco*)malloc(sizeof(Barco));
+    p=NULL;//(Persona*)malloc(sizeof(Persona));
+
+    while(decision)
+    {
+        switch(decision)
+        {
+            case 1:
+        
+                agregarBarco(&b);
+                break;       
+            case 2:
+                agregarPersona(&p);
+                break;
+
+            case 3:
+                agregarTrip(&p, &b);
+                break;
+
+            case 4:
+        
+                //printf("Numero de Barcos: %d", cuantosBarcos(b));
+                verBarcos(b);
+                break;
+            case 5:
+                verPersonas(p);
+                break;
+            case 6:
+                agregarProp(&p, &b);
+                break;
+        }
+    printf("Seleccione su opcion: \n1-Agregar Barco\n2-Agregar Persona\n3-Agregar Persona a Barco\n4-Mostrar Barcos\n5-Mostrar Personas\n6-Agregar Propietario\n0-Salir\n");
+    scanf("%d", &decision);
+    }    
+ 
+    freeMemory(p, b);  
+    //free(b);  
+    return 0;
+}
+
+
+
+
+void agregarProp(Persona ** p, Barco ** b)
+{
+    int persona;
+    int barco;
+
+    verPersonas(*p);
+    printf("Escoja la persona (numero):\n");
+    scanf("%d", &persona);
+    verBarcos(*b);
+    printf("Escoja el barco (numero)\n");
+    scanf("%d", &barco);
+    
+    Persona ** aux =  (&(*b + barco)->propietario);
+    if(!(cuantasPersonas(*aux)))
+    {
+        *aux = (Persona *)realloc(*aux,(cuantasPersonas(*aux)+1)*sizeof(Persona));
+
+        Persona * aux2 = *aux + cuantasPersonas(*aux)-1;
+        //aux2->nombre = malloc(sizeof(char)*10);
+
+        printf("%s\n",(*p+persona)->nombre);
+        aux2->nombre = (*p + persona)->nombre;
+        printf("%s\n\n",(aux2)->nombre);
+    	aux2->apellidos = (*p + persona)->apellidos;
+    	aux2->edad = (*p + persona)->edad;
+    	aux2->rol = (*p + persona)->rol;
+   
+        borrar(p,persona);
+    }
+    else
+        printf("Ya existe un propietario!\n");
+    
+}
+
+
+void agregarTrip(Persona ** p, Barco ** b)
+{
+    int persona;
+    int barco;
+
+    verPersonas(*p);
+    printf("Escoja la persona (numero):\n");
+    scanf("%d", &persona);
+    verBarcos(*b);
+    printf("Escoja el barco (numero)\n");
+    scanf("%d", &barco);
+    
+    Persona ** aux =  (&(*b + barco)->tripulacion);
+    if(cuantasPersonas(*aux)<(*b)->maximo)
+    {
+        *aux = (Persona *)realloc(*aux,(cuantasPersonas(*aux)+1)*sizeof(Persona));
+
+        Persona * aux2 = *aux + cuantasPersonas(*aux)-1;
+        //aux2->nombre = malloc(sizeof(char)*10);
+
+        printf("%s\n",(*p+persona)->nombre);
+        aux2->nombre = (*p + persona)->nombre;
+        printf("%s\n\n",(aux2)->nombre);
+    	aux2->apellidos = (*p + persona)->apellidos;
+    	aux2->edad = (*p + persona)->edad;
+    	aux2->rol = (*p + persona)->rol;
+   
+        borrar(p,persona);
+    }
+    else
+        printf("La tripulacion se encuentra llena!\n");
+    
+}
+void borrar(Persona ** p, int aBorrar)
+{
+    Persona * temp = (Persona *)malloc((cuantasPersonas(*p)-1)*sizeof(Persona));
+    memmove(temp, *p, (aBorrar)*sizeof(Persona));
+    memmove(temp + aBorrar, *p + (aBorrar+1), (cuantasPersonas(*p) - aBorrar - 1)*sizeof(Persona));
+    free (*p);
+    *p = temp;
+    
+}
+void agregarPersona(Persona ** p)
+{
+    Persona * aux;
+    //if(*p + cuantasPersonas(*p) - 1 != NULL)
+        *p = (Persona *)realloc(*p,(cuantasPersonas(*p)+1)*sizeof(Persona));
+    aux = *p + cuantasPersonas(*p) - 1;
+
+    (aux)->nombre = malloc(sizeof(char)*10);
+    printf("Ingrese el nombre:\n");
+    scanf("%s", (aux)->nombre);
+    (aux)->apellidos = malloc(sizeof(char)*10);
+    printf("Ingrese el apellido:\n");
+    scanf("%s", (aux)->apellidos);
+    printf("Cual es la edad: \n");
+    scanf("%d", &((aux)->edad));
+    (aux)->rol = malloc(sizeof(char)*10);
+    printf("Ingrese el rol:\n");
+    scanf("%s", (aux)->rol);
+    
+}
+
+void verPersonas(Persona *p)
+{
+    Persona * temp2 = p;
+    Persona * temp = p + cuantasPersonas(p);
+    for( ; p < temp; p++)
+    {
+        printf("\n\tPersona #%d\n\tNombre - %s\n\tApellido - %s\n\tEdad - %d\n\tRol - %s\n", p - temp2, p->nombre,p->apellidos,p->edad,p->rol);
+    }
+    
+}
+
+void agregarBarco(Barco ** b)
+{
+    Barco * aux;
+    *b = (Barco *)realloc(*b,(cuantosBarcos(*b)+1)*sizeof(Barco));
+    //free (*b);
+    aux = *b + cuantosBarcos(*b) - 1;
+
+    (aux)->nombre = malloc(sizeof(char)*10);
+    printf("Ingrese el nombre:\n");
+    scanf("%s", (aux)->nombre);
+    printf("Cual es la eslora: \n");
+    scanf("%d", &((aux)->eslora));
+    printf("Cual es la manga:\n");
+    scanf("%d", &(aux)->manga);
+    printf("Cual es el tamano de la tripulacion:\n");
+    scanf("%d", &(aux)->maximo);
+    //(aux)->tripulacion = NULL;//malloc(sizeof(Persona)*0);//(aux->maximo));
+    //(aux)->propietario = malloc(sizeof(Persona));//NULL;
+    //free(aux);
+}
+
+void verBarcos(Barco *b)
+{
+    Barco * temp2 = b;
+    Barco * temp = b + cuantosBarcos(b);
+    for( ; b < temp; b++)
+    {
+        printf("\n\n-------------------------------------------");
+        Persona * aux = b->tripulacion;
+        Persona * aux2 = b->propietario;
+        printf("\nBARCO #%d\n\tBarco - %s\n\tEslora - %d\n\tManga - %d\n\tMax. Trip - %d\nPlazas Disponibles - %d\n\n\tPROPIETARIO", b- temp2, b->nombre,b->eslora,b->manga,b->maximo, b->maximo - cuantasPersonas(aux));
+        verPersonas(aux2);
+        
+        printf("\n.......................................\n");
+        printf("\tTRIPULACION\n\n");
+        verPersonas(aux);
+        printf(".......................................\n");
+        printf("-------------------------------------------\n\n");
+        
+    }
+    
+    
+}
+
+
+int cuantosBarcos(Barco * b)
+{
+    return malloc_usable_size(b)/sizeof(Barco);
+}
+
+int cuantasPersonas(Persona * p)
+{
+    return malloc_usable_size(p)/sizeof(Persona);
+}
+
+void freeMemory(Persona * p, Barco * b)
+{
+    int i;
+    int j;
+    for(i = 0; i < cuantosBarcos(b); i++)
+    {
+        free((b+i)->nombre);
+       
+
+        Persona * aux =  ((b+i)->tripulacion);
+        Persona * aux2 = aux + cuantasPersonas(aux);
+        for ( j = 0; j <  cuantasPersonas(aux); j++)
+        {
+            free((aux+j)->nombre);
+            free((aux+j)->apellidos);
+            free((aux+j)->rol);
+           
+        }
+        free((b+i)->tripulacion);
+        //free((b->propietario)->nombre);
+        //free((b->propietario)->apellidos);
+        //free((b->propietario)->rol);
+        //free((b)->propietario);
+
+        //free((b)->nombre);
+        //free(b);
+        
+    }
+    free(b);
+
+    //free(eliminar);
+    //free(eliminar->nombre);
+
+    for( i = 0; i < cuantasPersonas(p); i++)
+    {
+        free((p+i)->nombre);
+        free((p+i)->apellidos);
+        free((p+i)->rol);
+    }
+    free(p);
+        
+}
---
+++ tarea1/ejercicio3.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#define N 0
+
+typedef struct
+{
+    char* nombre;
+    char* apellidos;
+    int edad;
+    char* telefono;
+    int cama;
+} Paciente;
+
+typedef struct
+{
+    Paciente * paciente;
+} Cama;
+
+void freeAll(Cama * c, Paciente * p, int pacientes, int camas);
+void quePacienteEnQueCama(Cama * c, int camas);
+void llegaPaciente (Paciente ** p, Cama ** c, int*, int*);
+int cuantosPacientes(Paciente * p);
+int cuantasCamas(Cama * c);
+void borrar(Paciente ** p, int aBorrar, int * pacientes);
+void verPacientes(Paciente * p, int pacientes);
+void contarCamas(Cama * c, int camas);
+
+main()
+{
+    
+
+    Paciente *p;
+    Cama *c;
+    
+    int decision = 1;
+    
+
+    p=NULL;//(Paciente*)malloc(0);
+    c=(Cama*)malloc(5 * sizeof(Cama));
+    int camas = 5;
+    int pacientes = 0;
+
+    while(decision)
+    {
+        printf("\nSeleccione su opcion: \n1-Llega un paciente\n2-Revisar Cama\n3-Dar de alta por cama\n4-Ver todos los pacientes\n5-Contar camas disponibles\n\n");
+        
+    
+        scanf("%d", &decision);
+        
+        switch(decision)
+        {
+            case 1:
+        
+                llegaPaciente(&p, &c, &pacientes, &camas);
+                break;  
+            case 2:
+                quePacienteEnQueCama(c, camas);
+                break;     
+            case 3:
+                darAltaPaciente(&p, &c, &pacientes, &camas);
+                break;
+            case 4:
+                verPacientes(p, pacientes);
+                break;
+            case 5:
+                contarCamas(c,camas);
+                break;
+        }
+        
+
+    }    
+    freeAll(c, p, pacientes, camas);
+    return 0;
+}
+
+
+void llegaPaciente (Paciente ** p, Cama ** c, int* pacientes, int* camas)
+{
+
+    Paciente * aux;;
+    *p = (Paciente *)realloc(*p,(*pacientes+1)*sizeof(Paciente));
+    *pacientes+=1;
+
+    aux = *p + *pacientes - 1;
+
+    (aux)->nombre = malloc(sizeof(char)*10);
+    printf("Ingrese el nombre:\n");
+    scanf("%s", (aux)->nombre);
+    (aux)->apellidos = malloc(sizeof(char)*10);
+    printf("Ingrese el apellido:\n");
+    scanf("%s", (aux)->apellidos);
+    printf("Cual es la edad: \n");
+    scanf("%d", &((aux)->edad));
+    (aux)->telefono = malloc(sizeof(char)*10);
+    printf("Ingrese el telefono:\n");
+    scanf("%s", (aux)->telefono);
+
+    
+    while(1)
+    {   
+        Cama * temp2;
+        Cama * temp = *c + *camas;
+        for( temp2 = *c; temp2 < temp; temp2++)   
+        {
+            if(temp2->paciente == NULL)
+            {
+                temp2->paciente = aux;
+                 printf("Se agrego el paciente #%d a la cama #%d\n", (aux - *p), temp2 - *c);
+                aux->cama=temp2-*c;
+                return;
+            }
+        }
+        
+        *c = (Cama *)realloc(*c,(*camas+5)*sizeof(Cama));  
+        printf("No hay suficientes camas, agregando 5 mas...\n");   
+        *camas+=5;   
+
+    }
+
+}
+
+void borrar(Paciente ** p, int aBorrar, int * pacientes)
+{
+    Paciente * temp = (Paciente *)malloc((*pacientes-1)*sizeof(Paciente));
+    memmove(temp, *p, (aBorrar)*sizeof(Paciente));
+    memmove(temp + aBorrar, *p + (aBorrar+1), (*pacientes - aBorrar - 1)*sizeof(Paciente));
+    *pacientes = *pacientes - 1;
+    free (*p);
+    *p = temp;
+    
+}
+
+void quePacienteEnQueCama(Cama * c, int camas)
+{
+    int numero;
+    printf("Que cama quiere ver?\n");
+    scanf("%d", &numero);
+    if(camas-numero > 1)
+    {
+        if((c+numero)->paciente == NULL)
+        {
+            printf("Esa cama esta vacia\n");
+            return;
+        }
+        printf("El paciente en esta cama es:\nNombre - %s\nApellido - %s\nEdad - %d\nTelefono - %s\n", (c+numero)->paciente->nombre, (c+numero)->paciente->apellidos, (c+numero)->paciente->edad, (c+numero)->paciente->telefono);
+    }
+    else
+        printf("Esa cama no existe\n");
+}
+
+void darAltaPaciente(Paciente ** p, Cama ** c, int * pacientes, int * camas)
+{
+    int numero;
+    printf("Que numero de cama desea dar de alta?\n");
+    scanf("%d", &numero);
+    if(camas-numero>1 & (*c+numero)->paciente != NULL)
+    {
+        borrar(p, numero, pacientes);
+        ((*c)+numero)->paciente = NULL;
+    }
+    else
+        printf("Esa cama no tiene paciente o no existe\n");
+}
+
+void verPacientes(Paciente * p, int pacientes)
+{
+    Paciente * inicio = p;
+    Paciente * final = p + pacientes;
+    for( ; p < final; p++)
+    printf("Paciente #%d\n\nNombre - %s\nApellido - %s\nEdad - %d\nTelefono - %s\nCama - %d\n",p - inicio, p->nombre, p->apellidos, p->edad, p->telefono, p->cama);
+}
+
+void contarCamas(Cama * c, int camas)
+{
+    int disponibles = 0;
+    
+    Cama* final = c + camas;
+    for( ; c < final; c++)
+    {
+        if(c->paciente == NULL)
+            disponibles++;
+    }	
+    printf("Camas disponibles: %d\nCamas ocupadas: %d\n", disponibles, camas-disponibles);
+}
+
+void freeAll(Cama * c, Paciente * p, int pacientes, int camas)
+{
+    int i;
+    /*for (i =0; i<camas; i++)
+    {
+        printf("hola\n");
+        free(((c+i)->paciente)->nombre);
+        free(((c+i)->paciente)->apellidos);
+        free(((c+i)->paciente)->telefono);
+        free((c+i)->paciente);
+    }*/
+    free(c);
+
+    for(i=0; i<pacientes;i++)
+    {
+        free((p+i)->nombre);
+        free((p+i)->apellidos);
+        free((p+i)->telefono);
+
+    }
+    free(p);
+}
